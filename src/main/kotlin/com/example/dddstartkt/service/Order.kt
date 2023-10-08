aebe064c5c33259968a2ebffea6027536ffaa036
mohadang <ehdlaos@gmail.com>
2023-10-08 18:58:17 +0900

    도메인 규칙 설정
    - 주문 취소는 출고 전에만 가능.
    - 배송지 변경은 출고 전에만 가능.

--- src/main/kotlin/com/example/dddstartkt/service/Order.kt
+++ src/main/kotlin/com/example/dddstartkt/service/Order.kt
@@ -6,6 +6,7 @@ enum class OrderState {
     SHIPPED,
     DELIVERING,
     DELIVERY_COMPLETED,
+    CANCELED,
 }
 
 class ShippingInfo(
@@ -18,12 +19,15 @@ class OrderLine(
     private var quantity: Int,
 ) {
     var amount: Int;
+
     init {
         this.amount = calculateAmounts()
     }
+
     fun isDifferentProduct(product: Product): Boolean {
         return this.product != product
     }
+
     private fun calculateAmounts(): Int {
         return this.price * this.quantity
     }
@@ -31,43 +35,54 @@ class OrderLine(
 
 class Order(
     private var orderState: OrderState,
-    private var shippingInfo: ShippingInfo, // 배송지 정보는 반드시 지정해야 한다.
+    private var shippingInfo: ShippingInfo, // Rule : 배송지 정보는 반드시 지정해야 한다.
     private var orderLines: List<OrderLine>, // 주문 집합.
     private var totalAmounts: Int,
 ) {
     init {
         setOrderLines(orderLines)
     }
+
     private fun setOrderLines(orderLines: List<OrderLine>) {
         verifyAtLeastOneOrMoreOrderLines(orderLines)
         this.orderLines = orderLines
         calculateTotalAmounts()
     }
+
     // 출고 상태로 변경
     fun changeShipped() {
         this.orderState = OrderState.SHIPPED
     }
+
     // 배송지 정보 변경
-    fun changeShippingInfo(changeShippingInfo: ShippingInfo) {
-        if (!this.isShippingChangeable()) {
-            throw IllegalStateException("배송중이 아닌 주문은 배송지를 변경할 수 없습니다.")
-        }
-        this.shippingInfo = changeShippingInfo
+    fun changeShippingInfo(newChangeShippingInfo: ShippingInfo) {
+        // Rule : 배송지 변경은 출고 전에만 가능.
+        verifyNotYetShipped()
+        this.shippingInfo = newChangeShippingInfo
     }
+
     // 주문 취소
     fun cancel() {
+        // Rule : 주문 취소는 출고 전에만 가능.
+        verifyNotYetShipped()
+        this.orderState = OrderState.CANCELED
     }
+
     // 결제 완료로 변경
-    private fun isShippingChangeable(): Boolean {
-        return this.orderState == OrderState.PAYMENT_WAITING ||
-                this.orderState == OrderState.PAYMENT_WAITING
+    private fun verifyNotYetShipped() {
+        if (this.orderState != OrderState.PAYMENT_WAITING &&
+            this.orderState != OrderState.PREPARING
+        )
+            throw IllegalStateException("aleady shipped")
     }
+
     // Rule : 최소 한 종류 이상의 상품을 주문해야 한다.
     private fun verifyAtLeastOneOrMoreOrderLines(orderLines: List<OrderLine>) {
         if (this.orderLines.isEmpty()) {
             throw IllegalArgumentException("최소 한 종류 이상의 상품이 있어야 합니다.")
         }
     }
+
     // Rule : 총 주문 금액은 각 상품의 구매 가격 합을 모두 더한 금액이다.
     private fun calculateTotalAmounts() {
         this.totalAmounts =
