836130623f2a959699b3cadb1ba7d2ae37ed2ee3
mohadang <ehdlaos@gmail.com>
2023-10-08 18:29:51 +0900

    Order에 cancel 함수 추가 및 주석

--- src/main/kotlin/com/example/dddstartkt/service/Order.kt
+++ src/main/kotlin/com/example/dddstartkt/service/Order.kt
@@ -33,17 +33,21 @@ class Order(
     private var orderState: OrderState,
     private var shippingInfo: ShippingInfo,
 ) {
+    // 출고 상태로 변경
+    fun changeShipped() {
+        this.orderState = OrderState.SHIPPED
+    }
+    // 배송지 정보 변경
     fun changeShippingInfo(changeShippingInfo: ShippingInfo) {
         if (!this.isShippingChangeable()) {
             throw IllegalStateException("배송중이 아닌 주문은 배송지를 변경할 수 없습니다.")
         }
         this.shippingInfo = changeShippingInfo
     }
-
-    fun changeShipped() {
-        this.orderState = OrderState.SHIPPED
+    // 주문 취소
+    fun cancel() {
     }
-
+    // 결제 완료로 변경
     private fun isShippingChangeable(): Boolean {
         return this.orderState == OrderState.PAYMENT_WAITING ||
                 this.orderState == OrderState.PAYMENT_WAITING
