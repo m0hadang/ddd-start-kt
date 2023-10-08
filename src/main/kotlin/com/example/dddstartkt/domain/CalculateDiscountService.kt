b9e6a0c2d46a98a23ff49bce932e212d23fff22f
mohadang <ehdlaos@gmail.com>
2023-10-08 21:46:22 +0900

    고수준, 저수준 모듈 관련 주석 추가

--- src/main/kotlin/com/example/dddstartkt/domain/CalculateDiscountService.kt
+++ src/main/kotlin/com/example/dddstartkt/domain/CalculateDiscountService.kt
@@ -1,14 +1,22 @@
 package com.example.dddstartkt.domain
 
-interface RuleDiscounter {
+/*
+고수준 모듈 : 여러 하위 기능을 조합해서 어떤 기능을 수행하는 모듈
+저수준 모듈 : 하위 기능을 구현한 모듈
+
+- 고수준 모듈은 저수준 모듈에 의존해서는 안된다.
+- 인터페이스로 추상화 하였어도 하위 레이어에 존재하는 모듈에 의존 하였다면 여전히 저수준 모듈에 의존하고 있느 것이다.
+* */
+interface RuleDiscounter { // 고수준 모듈
     fun applyRules(orderLines: List<OrderLine>): Money
 }
-class DroolsRuleDiscounter: RuleDiscounter {
+class DroolsRuleDiscounter: RuleDiscounter { // 저수준 모듈
     override fun applyRules(orderLines: List<OrderLine>): Money {
         return Money(0)
     }
 }
-class CalculateDiscountService(val ruleDiscounter: RuleDiscounter) {
+
+class CalculateDiscountService(val ruleDiscounter: RuleDiscounter) { // 고수준 모듈
     fun calculateDiscount(orderLines: List<OrderLine>, customerId: String): Money{
         return ruleDiscounter.applyRules(orderLines)
     }
