9bf3b025a4184c3c89bfeb5806e0579e5d589f07
mohadang <ehdlaos@gmail.com>
2023-10-08 13:08:53 +0900

    init

---
+++ src/main/kotlin/com/example/dddstartkt/DddStartKtApplication.kt
@@ -0,0 +1,11 @@
+package com.example.dddstartkt
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DddStartKtApplication
+
+fun main(args: Array<String>) {
+    runApplication<DddStartKtApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/dddstartkt/DddStartKtApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.dddstartkt
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DddStartKtApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
